77955256e1c272462f1dd9d06713f5e782d0ffe8
smart-prospective <81161080+smart-prospective@users.noreply.github.com>
2024-04-16 12:06:43 +0200

    Delete actions/fix_missing_messages_author_id.py

--- actions/fix_missing_messages_author_id.py
+++
@@ -1,9 +0,0 @@
-# In the Message model (mail.message) :
-
-for record in records:
-    if record.author_id:
-        partner_record = env['res.partner'].search([('id', '=', record.author_id.id)])
-        if not partner_record:
-            # find similar partner
-            record.write({'author_id':False})
-
